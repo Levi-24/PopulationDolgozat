f708a57727daa6d86592d3f708edb49439361e1d
Levente <122294490+Levi-24@users.noreply.github.com>
2024-12-10 10:23:31 +0100

    Add project files.

---
+++ Population/Allampolgar.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Population
+{
+    internal class Allampolgar
+    {
+        public int Id { get; set; }
+        public string Nem { get; set; }
+        public DateTime SzuletesiDatum { get; set; }
+        public string CsaladiAllapot { get; set; }
+        public string EredetiEgeszsegbiztositas { get; set; }
+        public bool Egeszsegbiztositas { get; set; }
+        public string EgeszsegbiztositasSzovegesen { get; set; }
+        public string EgeszsegugyiEllatas { get; set; }
+        public string EgeszsegugyiEllatasSzoveg { get; set; }
+        public string Nemzetiseg { get; set; }
+        public string Megye { get; set; }
+        public double HaviBruttoJovedelem { get; set; }
+        public double EvesEgyebBruttoJovedelem { get; set; }
+        public string IskolaiVegzettseg { get; set; }
+        public string IskolaiVegzettsegSzoveg { get; set; }
+        public string PolitikaiNezet { get; set; }
+        public bool Szavazokepes { get; set; }
+        public int TeaFogyasztasNaponta { get; set; }
+        public int FishAndChipsFogyasztasEvente { get; set; }
+        public string FishAndChipsFogyasztasEventeSzovegesen { get; set; }
+
+        public Allampolgar(string adatSor)
+        {
+            var temp = adatSor.Split(';');
+            Id = int.Parse(temp[0]);
+            Nem = temp[1];
+            SzuletesiDatum = DateTime.Parse(temp[2]).Date;
+            CsaladiAllapot = temp[3];
+            EredetiEgeszsegbiztositas = temp[4];
+            Egeszsegbiztositas = temp[4] == "van";
+            EgeszsegugyiEllatas = string.IsNullOrWhiteSpace(temp[5]) ? string.Empty : temp[5];
+            EgeszsegugyiEllatasSzoveg = string.IsNullOrWhiteSpace(temp[5]) ? "-" : temp[5];
+            Nemzetiseg = temp[6];
+            Megye = temp[7];
+            HaviBruttoJovedelem = double.TryParse(temp[8], out double havi) ? havi : -1;
+            EvesEgyebBruttoJovedelem = double.Parse(temp[9]);
+            IskolaiVegzettseg = string.IsNullOrWhiteSpace(temp[10]) ? string.Empty : temp[10];
+            IskolaiVegzettsegSzoveg = string.IsNullOrWhiteSpace(temp[10]) ? "-" : temp[10];
+            PolitikaiNezet = temp[11];
+            Szavazokepes = Szavazokepesseg();
+            TeaFogyasztasNaponta = int.Parse(temp[13]);
+            FishAndChipsFogyasztasEvente = temp[14] == "?" ? -1 : int.Parse(temp[14]);
+            FishAndChipsFogyasztasEventeSzovegesen = temp[14] == "?" ? "nincs adat" : temp[14];
+        }
+
+        public bool Szavazokepesseg()
+        {
+            return DateTime.Now > SzuletesiDatum.AddYears(21) ? true : false;
+        }
+
+        public double TeljesJovedelem()
+        {
+            return HaviBruttoJovedelem * 12 + EvesEgyebBruttoJovedelem;
+        }
+
+        public string Megjelenit(bool rovid)
+        {
+            if (rovid)
+            {
+                return $"{Id}\t{Nem}\t{SzuletesiDatum.ToShortDateString()}\t{CsaladiAllapot}\t{Egeszsegbiztositas}";
+            }
+            else
+            {
+                return $"{Id}\t{Nemzetiseg}\t{Megye}\t{HaviBruttoJovedelem:F2}\t{IskolaiVegzettseg}";
+            }
+        }
+    }
+}
---
+++ Population/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Population;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+
+namespace Population
+{
+    public partial class MainWindow : Window
+    {
+        private readonly List<Allampolgar> lakossag;
+        const int feladatokSzama = 5;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lakossag = new List<Allampolgar>();
+
+            using var sr = new StreamReader(@"..\..\..\SRC\population.txt");
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                lakossag.Add(new Allampolgar(sr.ReadLine()));
+            }
+
+            for (int i = 1; i <= feladatokSzama; i++)
+            {
+                feladatComboBox.Items.Add($"{i}.");
+            }
+
+            DataContext = this;
+            MegoldasTeljes.ItemsSource = lakossag;
+        }
+
+        private void FeladatComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MegoldasMondatos.Content = null;
+            MegoldasLista.Items.Clear();
+            MegoldasTeljes.ItemsSource = null;
+
+            var methodName = $"Feladat{feladatComboBox.SelectedIndex + 1}";
+            var method = GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+            method?.Invoke(this, null);
+        }
+
+        private void Feladat1()
+        {
+           var yorkshireNok = lakossag.Where(x => x.Nem == "nő" && x.Megye == "Yorkshire").ToList();
+           foreach (var szemely in yorkshireNok)
+                MegoldasLista.Items.Add(szemely.Megjelenit(false));
+        }
+
+        private void Feladat2()
+        {
+            MegoldasTeljes.ItemsSource = lakossag.OrderBy(x => x.SzuletesiDatum).Take(1);
+        }
+
+        private void Feladat3()
+        {
+            MegoldasTeljes.ItemsSource = lakossag.Where(x => !x.Egeszsegbiztositas);
+            //MegoldasTeljes.ItemsSource = lakossag.Where(x => x.EredetiEgeszsegbiztositas == "nincs");
+        }
+
+        private void Feladat4()
+        {
+           var legmagasabbAtlag = lakossag.GroupBy(p => p.Megye).Select(g => new
+          {
+              Megye = g.Key,
+              AtlagosBevetel = g.Average(p => p.HaviBruttoJovedelem * 12)
+          })
+          .OrderByDescending(g => g.AtlagosBevetel).FirstOrDefault();
+
+            MegoldasMondatos.Content = $"A legmagasabb átlagkeresetű megye: {legmagasabbAtlag.Megye}, az átlag éves jövedelem: {legmagasabbAtlag.AtlagosBevetel:0.00}font";
+        }
+
+        private void Feladat5()
+        {
+            var noiBevetelMegyenkent = lakossag.GroupBy(p => p.Megye).Select(g => new
+            {
+                Megye = g.Key,
+                LegmagasabbNoiBevetel = g.Where(x => x.Nem == "nő").Max(p => p.HaviBruttoJovedelem * 12)
+            });
+
+            MegoldasLista.ItemsSource = noiBevetelMegyenkent;
+        }
+    }
+}
